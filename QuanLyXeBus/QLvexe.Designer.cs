a98ec9930b9d3650a30bc8252e277ad81c288cf7
danhquyen2004 <mynamepow@gmail.com>
2024-03-05 22:24:06 +0700

    update

--- QuanLyXeBus/QLvexe.Designer.cs
+++ QuanLyXeBus/QLvexe.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace QuanLyXeBus
 {
-    partial class Form1
+    partial class QLvexe
     {
         /// <summary>
         /// Required designer variable.
@@ -35,10 +35,10 @@
             this.Tuyenxe = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Giave = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Soluongdaban = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
@@ -48,21 +48,22 @@
             this.groupBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.groupBox1.Controls.Add(this.textBox1);
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.groupBox1.Location = new System.Drawing.Point(12, 27);
+            this.groupBox1.Location = new System.Drawing.Point(3, 12);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(245, 52);
-            this.groupBox1.TabIndex = 1;
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(327, 64);
+            this.groupBox1.TabIndex = 7;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Tìm Kiếm Thông Tin Vé";
-            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(6, 22);
+            this.textBox1.Location = new System.Drawing.Point(8, 27);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(224, 22);
+            this.textBox1.Size = new System.Drawing.Size(297, 26);
             this.textBox1.TabIndex = 0;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
             // 
             // dataGridView1
             // 
@@ -72,22 +73,26 @@
             this.Tuyenxe,
             this.Giave,
             this.Soluongdaban});
-            this.dataGridView1.Location = new System.Drawing.Point(12, 102);
+            this.dataGridView1.Location = new System.Drawing.Point(3, 105);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(596, 226);
-            this.dataGridView1.TabIndex = 2;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick_1);
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(795, 278);
+            this.dataGridView1.TabIndex = 8;
             // 
             // Mave
             // 
             this.Mave.DataPropertyName = "Mave";
             this.Mave.HeaderText = "Mã Vé";
+            this.Mave.MinimumWidth = 6;
             this.Mave.Name = "Mave";
+            this.Mave.Width = 125;
             // 
             // Tuyenxe
             // 
             this.Tuyenxe.DataPropertyName = "Tuyenxe";
             this.Tuyenxe.HeaderText = "Tuyến xe";
+            this.Tuyenxe.MinimumWidth = 6;
             this.Tuyenxe.Name = "Tuyenxe";
             this.Tuyenxe.Width = 200;
             // 
@@ -95,6 +100,7 @@
             // 
             this.Giave.DataPropertyName = "Giave";
             this.Giave.HeaderText = "Giá Vé";
+            this.Giave.MinimumWidth = 6;
             this.Giave.Name = "Giave";
             this.Giave.Width = 150;
             // 
@@ -102,62 +108,63 @@
             // 
             this.Soluongdaban.DataPropertyName = "Soluongdaban";
             this.Soluongdaban.HeaderText = "Số lượng đã bán";
+            this.Soluongdaban.MinimumWidth = 6;
             this.Soluongdaban.Name = "Soluongdaban";
             this.Soluongdaban.Width = 150;
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(37, 343);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(106, 31);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Thêm";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(188, 343);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(106, 31);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "Sửa";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(340, 343);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(106, 31);
-            this.button3.TabIndex = 5;
-            this.button3.Text = "Xóa";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(488, 343);
+            this.button4.Location = new System.Drawing.Point(638, 401);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(106, 31);
-            this.button4.TabIndex = 6;
+            this.button4.Size = new System.Drawing.Size(141, 38);
+            this.button4.TabIndex = 12;
             this.button4.Text = "Làm Mới";
             this.button4.UseVisualStyleBackColor = true;
             // 
-            // Form1
+            // button3
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.button3.Location = new System.Drawing.Point(440, 401);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(141, 38);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Xóa";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(238, 401);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(141, 38);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Sửa";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(36, 401);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 38);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Thêm";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // QLvexe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.ClientSize = new System.Drawing.Size(640, 395);
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.groupBox1);
-            this.Name = "Form1";
-            this.Text = "Vé Xe";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Name = "QLvexe";
+            this.Text = "QLvexe";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
@@ -166,6 +173,7 @@
         }
 
         #endregion
+
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.DataGridView dataGridView1;
@@ -173,9 +181,9 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn Tuyenxe;
         private System.Windows.Forms.DataGridViewTextBoxColumn Giave;
         private System.Windows.Forms.DataGridViewTextBoxColumn Soluongdaban;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
     }
 }
