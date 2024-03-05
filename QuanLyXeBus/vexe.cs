8e0c6aaccc97bbd200cf5f955bfec32d578aafd5
danhquyen2004 <mynamepow@gmail.com>
2024-03-05 22:35:40 +0700

    rename vexe

--- QuanLyXeBus/vexe.cs
+++ QuanLyXeBus/vexe.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace QuanLyXeBus
 {
-    public partial class Form1 : Form
+    public partial class vexe : Form
     {
-        public Form1()
+        public vexe()
         {
             InitializeComponent();
         }
